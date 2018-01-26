e881a6aed5a08d6b47282fdd33b9c982c68128c7
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-26 02:34:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,14 +3,16 @@
 var chocolateBars = new Array('snickers, hundred grand, kitkat, and skittles')
 
 //TODO2
-function addElementToBeginningOfArray(array, element) {
 
 var cities = ["NYC", "San Francisco"];
 
-cities.unshift("Miami");
+function addElementToBeginningOfArray(array, element) {
+
+cities.unshift();
+return(cities);
 }
 
-addElementToBeginningOfArray()
+addElementToBeginningOfArray(cities,"Miami");
 
 
 
